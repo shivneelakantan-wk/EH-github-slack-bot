afa451a0f277fdecb8c1387f930350046b7fbef0
shivneelakantan-wk <shiv.neelakantan@workiva.com>
2025-06-05 21:30:07 -0500

    wip

--- get_pr_metadata.py
+++ get_pr_metadata.py
@@ -3,6 +3,7 @@ import os
 from github import Github
 from github.Auth import Token
 
+
 class PR_DTO:
     def __init__(self, pr_title, pr_author_username, pr_url, pr_date):
         self.pr_title = pr_title
@@ -10,14 +11,14 @@ class PR_DTO:
         self.pr_url = pr_url
         self.pr_date = pr_date
 
-def get_open_pull_requests_for_repo(repo_owner, repo_name, team_member_usernames):
+
+def _get_open_pull_requests_for_repo(repo_owner, repo_name, team_member_usernames):
     '''
     fetches and returns details of all open PRs for a given github repo.
     :param repo_owner:
     :param repo_name:
     :return:
     '''
-    load_dotenv()
     github_token = os.getenv('GITHUB_PERSONAL_ACCESS_TOKEN')
 
     if not github_token:
@@ -36,26 +37,26 @@ def get_open_pull_requests_for_repo(repo_owner, repo_name, team_member_usernames
         # get all open PRs
         open_prs = repository.get_pulls(state='open')
 
-        teams_prs = []
+        teams_prs_for_repo = []
 
         for pr in open_prs:
             pr_title = pr.title
             pr_author_username = pr.user.login
 
-            if pr_author_username not in team_member_usernames:
+            if pr_author_username.strip() not in team_member_usernames:
                 continue
 
             pr_id = pr.url.split('/')[-1]
-            pr_url = f'https://github.com/{repo_owner}/{repo_name}/pull/{pr_id}'
+            pr_url = pr.html_url
             pr_date = pr.created_at.strftime('%Y-%m-%d %H:%M:%S')
 
             pr_object = PR_DTO(pr_title, pr_author_username, pr_url, pr_date)
-            teams_prs.append(pr_object)
+            teams_prs_for_repo.append(pr_object)
 
-        return teams_prs
+        return teams_prs_for_repo
 
-    except:
-        pass
+    except Exception as e:
+        print(f'ERROR: could not fetch open PRs for {repo_owner}/{repo_name} - {e}')
 
 
 def get_open_pull_requests_for_list_of_repos(repo_owner, repo_names, team_member_usernames):
@@ -66,16 +67,33 @@ def get_open_pull_requests_for_list_of_repos(repo_owner, repo_names, team_member
     :param repo_names:
     :return:
     '''
-    event_horizon_prs = []
+    teams_prs = []
     for repo_name in repo_names:
-        prs = get_open_pull_requests_for_repo(repo_owner, repo_name, team_member_usernames)
+        prs = _get_open_pull_requests_for_repo(repo_owner, repo_name, team_member_usernames)
         if prs:
-            event_horizon_prs.extend(prs)
+            teams_prs.extend(prs)
+    print(f'found {len(teams_prs)} open PRs for {repo_owner} in repos {repo_names}')
+    return teams_prs
 
-    return event_horizon_prs
+def send_slack_message(channel_id, message_text, client):
+    '''
+    Sends a message to a Slack channel.
+    :param channel_id: ID of the Slack channel
+    :param message_text: Text of the message to send
+    :param client: Slack client instance
+    '''
+    try:
+        response = client.chat_postMessage(channel=channel_id, text=message_text)
+        print(f'Message sent successfully: {response["ts"]}')
+    except Exception as e:
+        print(f'Error sending message: {e}')
+
 
 if __name__ == '__main__':
-    get_open_pull_requests_for_list_of_repos('shivneelakantan-wk',
-                                             ['EH-github-slack-bot'],
-                                             set('shivneelakantan-wk')
-                                             )
+    load_dotenv()
+    prs = get_open_pull_requests_for_list_of_repos('shivneelakantan-wk',
+                                                   ['EH-github-slack-bot'],
+                                                   {'shivneelakantan-wk'}
+                                                   )
+    for pr in prs:
+        print(f'Title: {pr.pr_title}, Author: {pr.pr_author_username}, URL: {pr.pr_url}, Date: {pr.pr_date}')
---
+++ slack_bot.py
@@ -0,0 +1,62 @@
+import os
+from dotenv import load_dotenv
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+from get_pr_metadata import PR_DTO, _get_open_pull_requests_for_repo, get_open_pull_requests_for_list_of_repos
+
+
+def send_slack_message(channel_id, message_text, client):
+    try:
+        response = client.chat_postMessage(
+            channel=channel_id,
+            text=message_text
+        )
+
+        print(f"Message sent successfully to channel {channel_id}: {response['message']['text']}")
+        return True
+
+    except SlackApiError as e:
+        print(f"Error sending message to channel {channel_id}: {e.response['error']}")
+        return False
+
+def format_prs_for_slack(pr_dto_list, repo_owner, repo_name):
+    """
+    Formats a list of PR DTOs into a Slack message.
+    :param pr_dto_list: List of PR DTOs
+    :param repo_owner: Owner of the repository
+    :param repo_name: Name of the repository
+    :return: Formatted message string
+    """
+    if not pr_dto_list:
+        return f"No open PRs found for {repo_owner}/{repo_name}."
+
+    message_lines = [f"Open PRs for *{repo_owner}/{repo_name}*:\n"]
+
+    for pr in pr_dto_list:
+        message_lines.append(f"- *{pr.title}* by @{pr.author_username} - <{pr.url}|View PR> (Created on {pr.date})")
+
+    return "\n".join(message_lines)
+
+if __name__ == '__main__':
+    # load_dotenv()  # Load environment variables from .env file
+    # slack_bot_token = os.getenv('SLACK_BOT_TOKEN')
+    # slack_channel_id = os.getenv('SLACK_CHANNEL_ID')
+    #
+    # if not slack_bot_token:
+    #     print("Error: SLACK_BOT_TOKEN not found in environment variables.")
+    #     exit(1)
+    #
+    # client = WebClient(token=slack_bot_token)
+    #
+    # # Example usage
+    # channel_id = 'C1234567890'  # Replace with your Slack channel ID
+    # message_text = "Hello, Slack!"
+    #
+    # if send_slack_message(channel_id, message_text, client):
+    #     print("Message sent successfully.")
+    # else:
+    #     print("Failed to send message.")
+    all_team_prs = get_open_pull_requests_for_list_of_repos('shivneelakantan-wk',
+                                                   ['EH-github-slack-bot'],
+                                                   {'shivneelakantan-wk'}
+                                                   )
